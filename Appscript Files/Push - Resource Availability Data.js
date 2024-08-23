2edbf063a36739c25d71ce01567e2d7565088d2a
Ivan Lee <ivanlee2345@gmail.com>
2024-08-23 15:24:55 +0800

    Delete Appscript Files directory

--- Appscript Files/Push - Resource Availability Data.js
+++
@@ -1,9 +0,0 @@
-function doGet() {
-  return ContentService.createTextOutput(JSON.stringify(getData())).setMimeType(ContentService.MimeType.JSON);
-}
-
-function getData() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Resources");
-  var data = sheet.getDataRange().getValues();
-  return data;
-}
--- Appscript Files/Push - Task Data.js
+++
@@ -1,6 +0,0 @@
-function doGet() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Task Management');
-  var data = sheet.getDataRange().getValues();
-  var jsonData = JSON.stringify(data);
-  return ContentService.createTextOutput(jsonData).setMimeType(ContentService.MimeType.JSON);
-}
